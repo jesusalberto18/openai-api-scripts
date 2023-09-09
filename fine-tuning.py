f77acb6170a7660911726f3b42fe636312140203
Alberto Hernández <albertiae.digital@gmail.com>
2023-09-09 09:20:37 -0500

    Fine-Tuning.py

---
+++ fine-tuning.py
@@ -0,0 +1,17 @@
+# Create a fine-tuning job with an already uploaded file
+openai.FineTuningJob.create(training_file="file-abc123", model="gpt-3.5-turbo")
+
+# List 10 fine-tuning jobs
+openai.FineTuningJob.list(limit=10)
+
+# Retrieve the state of a fine-tune
+openai.FineTuningJob.retrieve("ft-abc123")
+
+# Cancel a job
+openai.FineTuningJob.cancel("ft-abc123")
+
+# List up to 10 events from a fine-tuning job
+openai.FineTuningJob.list_events(id="ft-abc123", limit=10)
+
+# Delete a fine-tuned model (must be an owner of the org the model was created in)
+openai.Model.delete("ft-abc123")
